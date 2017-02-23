29f4aace759935d6dd0cfc2763a812e870612a8b
sabruss <sabruss@users.noreply.github.com>
2017-02-23 14:38:14 +0100

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -7,12 +7,12 @@ exports.run = (client, message, params) => {
         if (!params[0]) {
             const commandNames = Array.from(client.commands.keys());
             const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-            message.channel.sendCode('asciidoc', `= Liste des commandes =\n\n[Utilise ${guild.prefix}help <nomdelacommande> pour plus de details]\n\n${client.commands.filter(c => client.elevation(message) >= c.conf.permLevel).map(c => `${guild.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description} ${c.conf.enabled ? '' : '[Commande désactivée]'}`).join('\n')}`);
+            message.channel.sendCode('asciidoc', `= Liste des commandes =\n\n[Utilise ${guild.prefix}help <nomdelacommande> pour plus de details]\n\n${client.commands.filter(c => client.elevation(message.member) >= c.conf.permLevel).map(c => `${guild.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description} ${c.conf.enabled ? '' : '[Commande désactivée]'}`).join('\n')}`);
         } else {
             let command = params[0];
             if (client.commands.has(command)) {
                 command = client.commands.get(command);
-                if (client.elevation(message) < command.conf.permLevel) return;
+                if (client.elevation(message.member) < command.conf.permLevel) return;
                 message.channel.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nUsage:  ${guild.prefix}${command.help.usage}${command.conf.aliases.length ? `\nAliases: ${command.conf.aliases.join(",")}` : ''}`);
             }
         }
--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,26 +1,38 @@
 const Discord = require("discord.js")
+
+const level_alias = {
+    0: "Membre",
+    1: "Modérateur",
+    2: "Administrateur",
+    3: "Server Owner",
+    4: "Bot Owner"
+}
+
 exports.run = (client, message, params) => {
-                if (message.mentions.users.size < 1) {
-                    let embed = new Discord.RichEmbed().setTitle(`Information pour ce membre`).setColor("#51adf6").setThumbnail(message.author.avatarURL)
-                    embed.addField("Nom", message.author.username + "#" + message.author.discriminator + " (" + message.author.id + ")")
-                    embed.addField("Alias", message.member.nickname)
-                    embed.addField("Rejoind le server", message.member.joinedAt)
-                    embed.addField("R\364les", message.member.roles.array().splice(1).map(r =>r.name).join(", ") ? message.member.roles.array().splice(1).map(r =>r.name).join(", ") : "Aucun role assigné")
-                    embed.addField("Status", message.member.user.presence.status)
-                    return message.channel.sendEmbed(embed).catch(console.error);
-                } else {
-                    let guildMember = message.guild.member(message.mentions.users.first());
-                    if (!guildMember) return message.reply("Utilisateur inconnu.");
-                    let embed = new Discord.RichEmbed().setColor("#51adf6").setThumbnail(guildMember.user.avatarURL)
-                    embed.setTitle(`Information pour le membre ${guildMember.user.username}`)
-                    embed.addField("Nom", guildMember.user.username + "#" + guildMember.user.discriminator + " (" + guildMember.id + ")")
-                    embed.addField("Alias", guildMember.nickname)
-                    embed.addField("Rejoind le server", guildMember.joinedAt)
-                    embed.addField("R\364les", guildMember.roles.array().splice(1).map(r =>r.name).join(", ") ? guildMember.roles.array().splice(1).map(r =>r.name).join(", ") : "Aucun role assigné")
-                    embed.addField("Status", guildMember.user.presence.status)
-                    embed.setColor("#51adf6");
-                    return message.channel.sendEmbed(embed).catch(console.error);
-                }
+
+    if (message.mentions.users.size < 1) {
+        console.log()
+        let embed = new Discord.RichEmbed().setTitle(`Information pour ce membre`).setColor("#51adf6").setThumbnail(message.author.avatarURL)
+        embed.addField("Nom", message.author.username + "#" + message.author.discriminator + " (" + message.author.id + ")")
+        embed.addField("Alias", message.member.nickname)
+        embed.addField("Rejoind le server", message.member.joinedAt)
+        embed.addField("R\364les", message.member.roles.array().splice(1).map(r =>r.name).join(", ") ? message.member.roles.array().splice(1).map(r =>r.name).join(", ") : "Aucun role assigné")
+        embed.addField("Status", message.member.user.presence.status)
+        embed.addField("Niveau de permission", client.elevation(message.member) + " | " + level_alias[client.elevation(message.member)])
+        return message.channel.sendEmbed(embed).catch(console.error);
+    } else {
+        let guildMember = message.guild.member(message.mentions.users.first());
+        if (!guildMember) return message.reply("Utilisateur inconnu.");
+        let embed = new Discord.RichEmbed().setColor("#51adf6").setThumbnail(guildMember.user.avatarURL)
+        embed.setTitle(`Information pour le membre ${guildMember.user.username}`)
+        embed.addField("Nom", guildMember.user.username + "#" + guildMember.user.discriminator + " (" + guildMember.id + ")")
+        embed.addField("Alias", guildMember.nickname)
+        embed.addField("Rejoind le server", guildMember.joinedAt)
+        embed.addField("R\364les", guildMember.roles.array().splice(1).map(r =>r.name).join(", ") ? guildMember.roles.array().splice(1).map(r =>r.name).join(", ") : "Aucun role assigné")
+        embed.addField("Status", guildMember.user.presence.status)
+        embed.addField("Niveau de permission", client.elevation(guildMember) + " | " + level_alias[client.elevation(guildMember)])
+        return message.channel.sendEmbed(embed).catch(console.error);
+    }
 };
 
 exports.conf = {
